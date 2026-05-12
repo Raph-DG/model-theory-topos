249abcb8384ee0361215fc2419294749ea180648
FernandoChu <17756312+FernandoChu@users.noreply.github.com>
2026-05-12 17:38:26 +0200

    explanation

--- ModelTheoryTopos/Geometric/Examples/KripkeJoyal.lean
+++ ModelTheoryTopos/Geometric/Examples/KripkeJoyal.lean
@@ -14,7 +14,22 @@ The interpretation of the forcing relation `U ⊩ φ(α)` ("U forces φ") is a m
 that `s ≫ φ = α`.
 
 This can be interpreted into our system, at least when `α` is a mono. Indeed, then it induces a
-subobject, which we could interpret as part of our `FormulaContext`. This file does precisely that.
+subobject `U`, which we could interpret as part of our `FormulaContext`. Hence, we can write `U ⊢ᵈ φ`
+for `U ⊩ φ(α)`.
+
+To do this, however, we need to hardcode the subobject `U → X` as part of our signature. That is,
+extend the signature with a new sort `X'`, and a new property `U'` defined on `X'`. The `Structure`
+of this new signature then maps the sort `X` to `X'` and `U'` to the subobject `U`.
+
+Furthermore, because our syntax allows for formulas to be assumed in the context, we can express the
+mathematical content of implications and foralls. Indeed, instead of `U ⊩ φ → ψ` we can instead write
+`U, φ ⊢ᵈ ψ`; while instead of `U ⊩ ∀ y : Y, φ(y)` we can write `U* ⊢ᵈ φ`, where `U*` is the subobject
+over `X × Y` given by pulling back.
+
+U* ----> φ     U ----> ∀ y : Y, φ(y)
+  \     /       \     /
+   \   /         \   /
+   X × Y ------->  X
 -/
 
 universe w v u
@@ -35,7 +50,7 @@ def StructureExtend : Structure S.ExtendOneSymbol C where
   sorts X := match X with
   | .some X => ⟦M|X⟧ˢ
   | .none => X'
-  Functions f := M.Functions f
+  Functions f := by push_neg
   Relations := sorry
 
 -- Extend interpretations to an interpretation of ExtendOneSymbol
